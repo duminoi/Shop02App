60993f65d6d54efcbaf75f84e378de1de0cd1d7e
duminoi <dminso1koaiso2@gmail.com>
2024-09-06 01:42:20 +0700

    u

--- src/components/Details.jsx
+++ src/components/Details.jsx
@@ -3,15 +3,45 @@ import "../assets/css/details.css";
 import { useDispatch, useSelector } from "../store/hook";
 import accounting from "accounting";
 import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
 export default function Details() {
   const detail = useSelector((state) => state.details);
   console.log("vào details");
-
+  const cart = useSelector((state) => state.cart);
+  const productList = useSelector((state) => state.productList);
   const dispatch = useDispatch();
   console.log(detail);
   const navigate = useNavigate();
   const handleClick = () => {
     navigate("/");
+  };
+  const handleCart = (e) => {
+    const id = detail._id;
+    console.log(id);
+    const index = cart.findIndex(({ _id }) => {
+      return id == _id;
+    });
+    console.log(cart);
+    productList.forEach((item) => {
+      if (item._id === id) {
+        if (index <= -1) {
+          const newCart = { ...item, count: 1 };
+          console.log(newCart);
+          dispatch({ type: "cart/add", payload: newCart });
+          // dispatch({ type: "toastContent", payload: "idle" });
+          // dispatch({ type: "toast", payload: `${item.name} is added` });
+          toast(`${item.name} is added`);
+        } else {
+          const newCart = [...cart];
+          newCart[index].count += 1;
+          localStorage.setItem("cart", JSON.stringify(newCart));
+          // dispatch({ type: "toastContent", payload: "idle" });
+          // dispatch({ type: "toast", payload: `${item.name} is added` });
+          toast(`${item.name} is added`);
+        }
+      }
+    });
+    console.log(index);
   };
   return (
     <div id="single-product-container" className="mt-[4rem] container">
@@ -32,7 +62,7 @@ export default function Details() {
               <span>$</span>
               {accounting.formatMoney(detail.price, "", 0)}
             </h2>
-            <button>Add to cart</button>
+            <button onClick={handleCart}>Add to cart</button>
           </div>
         </div>
       </div>
